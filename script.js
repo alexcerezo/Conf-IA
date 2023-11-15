f9fbdcde516f7346bff507d9a1138f5883ad5522
Alex Cerezo <alexcerezocontreras@gmail.com>
2023-11-15 20:25:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  function showMenu() {
-      if (window.innerWidth < 1000) {
-          var menu = document.querySelector('.menu');
-          if (menu) menu.style.display = 'flex';
-      }
+// script.js
+function showMenu() {
+    var menu = document.querySelector('.menu');
+    if (menu) menu.style.display = 'flex';
   }
-
+  
   function hideMenu() {
-      if (window.innerWidth < 1000) {
-          var menu = document.querySelector('.menu');
-          if (menu) menu.style.display = 'none';
-      }
+    var menu = document.querySelector('.menu');
+    if (menu) menu.style.display = 'none';
   }
-
-  if (window.innerWidth > 1000) {
-      var menu = document.querySelector('.menu');
+  
+  window.addEventListener('resize', function() {
+    var menu = document.querySelector('.menu');
+    if (window.innerWidth > 1000) {
       if (menu) menu.style.display = 'flex';
-  }
-});
+    } else {
+      if (menu) menu.style.display = 'none';
+    }
+  });
+  
+  document.addEventListener('DOMContentLoaded', (event) => {
+    var menu = document.querySelector('.menu');
+    if (window.innerWidth > 1000) {
+      if (menu) menu.style.display = 'flex';
+    } else {
+      if (menu) menu.style.display = 'none';
+    }
+  });
